703919e79b389a731a563ecdc98a5fb69c56c832
surajpatwal22 <patwal0suraj@gmail.com>
2024-01-27 23:33:15 +0530

    sidebar

---
+++ script.js
@@ -0,0 +1,16 @@
+const body = document.querySelector("body"),
+      sidebar = document.querySelector(".sidebar"),
+      toggle = document.querySelector(".toggle"),
+      searchBtn = document.querySelector(".search-box"),
+      modeSwitch = document.querySelector(".toggle-switch"),
+      modeText = document.querySelector(".mode-text");
+
+
+      toggle.addEventListener('click',()=>{
+        sidebar.classList.toggle("close");
+        console.log(toggle);
+      });
+
+      modeSwitch.addEventListener('click',()=>{
+        body.classList.toggle("dark");
+      });
